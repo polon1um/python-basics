53385476891b0130e1a76f650bd7b7d8390540c4
momonji <anarahbohte@mail.ru>
2020-09-14 10:36:23 +0500

    student_marks

--- 01.list.py
+++ 01.list.py
@@ -1,14 +1,40 @@
 import sys
 
-my_list = [12, '15', 95.32, 55, True]
-print(my_list, type(my_list), id(my_list), sys.getsizeof(my_list))
+# to contain -> container -> arrays
+# to read -> reader
+
+# [12, 15, 95, 55, 36]
+
+# !!! all is object !!! OOP
+# объекты (пользователь)
+#     - атрибуты: свойства (id, name, login, password)
+#     - методы: действия (edit, delete, create, input_data)
+
+# snake_case
+# My_var
+# my_Var
+# myVar -> cameCase (Java, JS)
+
+# 'ya.ru' -> reference
+# my_var = [12, '15', 95.32, 55, True]  # var -> reference
+# print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
+#
+# my_var = 12
+# print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
+#
+# my_var = 12.5
+# print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
+#
+# my_var = '12.5'
+# print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
 
 student_marks = []
 while True:
-    mark = input('введите оценку студента:' )
-    if mark: # '' == false, 0=[ false, [] == false
+    mark = input('Введите оценку студента: \n')
+    if mark:
         student_marks.append(mark)
     else:
         break
-print('ввод завершен')
+
+print('Ввод завершен')
 print(student_marks)
