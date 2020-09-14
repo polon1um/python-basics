b0567adc2aceb1f7a454f5a5579dba6dfdf0614f
momonji <anarahbohte@mail.ru>
2020-09-14 12:38:55 +0500

    enumerate

--- 03.list.py
+++ 03.list.py
@@ -7,11 +7,38 @@ lesson_dates = [
 ]
 student_marks = [5, 4, 3, 2, 5]
 
-# len(student_marks) -> 5
-i = 0
-while i < len(student_marks):
-    # i -> 0, 0 < 5 -> True ?
-    # i -> 1, 1 < 5 -> True ?
-    # i -> 5, 5 < 5 -> False ?
-    print(lesson_dates[i], 'оценка', student_marks[i])
-    i += 1  # i = 1
+# i = 0
+# while i < len(student_marks):
+#     print(lesson_dates[i], 'оценка', student_marks[i])
+#     i += 1
+
+# for item in student_marks:  # item
+#     print('оценка', item)
+#
+# for item in enumerate(student_marks):  # pairs -> (num, item)
+#     print('оценка', item)
+
+for item in enumerate(student_marks):  # pairs -> (num, item)
+    i, mark = item
+    print('оценка', item, 'или', i, mark)
+
+# user_full_name = ['Иван', 'Иванов']
+# # first_name = user_full_name[0]
+# # second_name = user_full_name[1]
+#
+# # first_name, second_name = ['Иван', 'Иванов']
+# first_name, second_name = user_full_name
+#
+# print(first_name, second_name)
+#
+# a, b, c, d, e = [15, 45, 87, 96, 4]
+# print(a, b, c, d, e)
+
+
+# feature
+# in place
+for i, mark in enumerate(student_marks):  # pairs -> (num, mark)
+    print('оценка', i, mark)
+    # print(lesson_dates[i], 'оценка', item)
+
+# i, mark = item -> i, mark = (0, 5) -> i = 0, mark = 5
